3e28ba4e36ee2f59b2c9e144c6966c1aa36a9d8a
Angelika Brown <angelikpbrown@gmail.com>
2025-04-13 01:30:43 -0400

    py demo

--- PythonDemo/src/main/python/DemoPython/Main.py
+++ PythonDemo/src/main/python/DemoPython/Main.py
@@ -8,9 +8,15 @@ class Main:
             list3.append(i ** n)
         print(list3)
 
-    def user_input(self, message):
+    def remove_evens(self, n):
+        lista = [1, 2, 3, 4, 5, 6, 7, 8]
         pass
 
+    def reverse(self, a):
+        pass
+
+    def palindrome(self):
+        pass
 
 demo_exponent = Main()
 demo_exponent.exponential(2)
