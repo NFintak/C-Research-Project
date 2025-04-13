3f9148870512f3709032a0f6201ff55242ee0e78
Angelika Brown <angelikpbrown@gmail.com>
2025-04-13 16:11:38 -0400

    Python methods done

--- PythonDemo/src/main/python/DemoPython/Main.py
+++ PythonDemo/src/main/python/DemoPython/Main.py
@@ -1,6 +1,9 @@
 class Main:
     def __init__(self):
         pass
+
+
+
     def exponential(self, n):
         lista = [1, 2, 3, 4, 5, 6, 7, 8]
         list3 = []
@@ -8,15 +11,64 @@ class Main:
             list3.append(i ** n)
         print(list3)
 
-    def remove_evens(self, n):
+
+
+    def remove_odds(self):
         lista = [1, 2, 3, 4, 5, 6, 7, 8]
-        pass
+        for item in lista:
+            if item % 2 != 0:
+                lista.remove(item)
+            print(lista)
+
+
 
     def reverse(self, a):
-        pass
+        print (a[::-1])
 
-    def palindrome(self):
-        pass
 
-demo_exponent = Main()
-demo_exponent.exponential(2)
+    def palindrome(self, value ):
+        sanitized_input = value.lower().replace(" ", "")
+        reversed_sanitized_input = reversed(sanitized_input)
+
+        a = list(sanitized_input)
+        b = list(reversed_sanitized_input)
+
+        print (a == b)
+
+    def greet(self, name):
+        """
+        This function receives a name parameter and utilizes it to print a greeting.
+        """
+        print("Hello, "+ name)
+
+    def name_input(self):
+        """
+        This function prompts the user for their name and returns the value provided by the user.
+        """
+        return input("What is your name?\n")
+
+
+
+
+
+#exponential
+#demo_one = Main()
+#demo_one.exponential(2)
+
+#removeodds
+#demo_two = Main()
+#demo_two.remove_odds()
+
+#reverse
+#demo_three = Main()
+#demo_three.reverse('applesauce')
+
+#palnindrome
+#demo_four = Main()
+#demo_four.palindrome('Dog')
+#demo_four.palindrome('Racecar')
+
+#greet
+#demo_five = Main()
+#name = demo_five.name_input()
+#demo_five.greet(name)
