3dd7261cff54a7d410deaa988d5720edf279690d
Angelika Brown <angelikpbrown@gmail.com>
2025-04-14 18:33:19 -0400

    Update Main.py

--- PythonDemo/src/main/python/DemoPython/Main.py
+++ PythonDemo/src/main/python/DemoPython/Main.py
@@ -3,18 +3,26 @@ class Main:
         pass
 
     def exponential(self, n):
+        # create your list of numbers
         lista = [1, 2, 3, 4, 5, 6, 7, 8]
+        #empty list to hold results
         list3 = []
+        #begin your loop
         for i in lista:
             list3.append(i ** n)
+        #print the results
         print(list3)
 
 
     def remove_odds(self):
+        #create your list of numbers
         lista = [1, 2, 3, 4, 5, 6, 7, 8]
+        #setup your loop
         for item in lista:
+            #remove items that are odd
             if item % 2 != 0:
                 lista.remove(item)
+            #print the results
             print(lista)
 
 
@@ -23,16 +31,15 @@ class Main:
 
 
     def palindrome(self, value ):
+        #take value and clean it -- make all lowercase and remove spaces
         clean_input = value.lower().replace(" ", "")
+        #reverse the clean string
         reversed_clean_input = reversed(clean_input)
-
+        #place clean and reverse string into variables
         a = list(clean_input)
         b = list(reversed_clean_input)
-
+        #print true if a == b
         print (a == b)
-
-
-
 
 
 
